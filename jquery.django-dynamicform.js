7a17e9fb9e8a6ab5fd42fe620f2d3176e0131cf2
bturner <bturner@61f19ded-3748-0410-a67b-fd36171a9782>
2009-01-19 08:19:48 +0000

    with help from xian, the add-remove functionality works, ur doin it wrong, ben
    git-svn-id: https://svn.ben-turner.com/hogback@182 61f19ded-3748-0410-a67b-fd36171a9782

--- jquery.django-dynamicform.js
+++ jquery.django-dynamicform.js
@@ -26,135 +26,53 @@
                 var target = $(e.target);
                                 
                 if (target.hasClass(options.add_link_class)) {
-                    target.trigger('add_field');
+                    obj.trigger('addField');
                 }
                 if (target.hasClass(options.remove_link_class)) {
-                    target.trigger('remove_field');
+                    obj.trigger('removeField', [target]);
                 }
             });
             
-            obj.bind('add_field.dynamicform', add_field);
-            obj.bind('remove_field.dynamicform', remove_field);
+            obj.bind('addField.dynamicform', addField);
+            obj.bind('addField.dynamicform', handleFieldUpdate);
             
-            // obj.bind('add_field.dynamicform', fields_changed);
+            obj.bind('removeField.dynamicform', removeField);
+            obj.bind('removeField.dynamicform', handleFieldUpdate);
             
-            function add_field(e) {
-                debug('add_field(): add_field() was called by ' + $(e.target).attr("class"));
-                // var target = $(e.target);                
-                var clone = $(obj).clone(true);
+            function addField(e) {
+                // Add a new field
+                debug('addField() was executed.');
                 
-                clone.find('.' + options.add_link_class).after(' ' + remove_link);
-                
-                obj.after(clone);
-                
-                $(obj.get_class()).bind('remove_field.dynamicform', fields_changed);                
+                clone = obj.children('table:last').clone(true);
+                clone.find('input')
+                obj.append(clone);
             }
             
-            function remove_field(e) {
-                debug('remove_field(): remove_field() was called by ' + $(e.target).attr("class"));
+            function removeField(e, target) {
+                // Remove a field
+                debug('removeField() was executed.');
+                debug(target);
                 
-                var target = $(e.target);
-                
-                target.parents().filter(obj.get_class()).prev().css("background", "green");
-                
-                debug(target.parents().filter(obj.get_class()).prev().find('.' + options.add_link_class).attr("class"));
-                
-                var parent = target.parents().filter(obj.get_class());
-                parent.next().css("background", "red");
-                
-                debug(parent.prev());
-                debug(parent.next());
-                debug(obj.count());
-                
-                if ((parent.prev() || parent.next() != 'table')) {
-                    debug('Check it.');
-                }
-                
-                if (parent.prev().find('.' + options.add_link_class).attr("class")) {
-                    debug('Found the add-link class');
-                } else {
-                    if ((parent.next()) != ($('table'))) {
-                        target.parents().filter(obj.get_class()).prev().find('.' + options.remove_link_class).before(add_link + ' ');
-                    }
-                }
-                
-                // if (!($(this).prev().is(':first'))) {
-                //     $(this).prev().find('.' + options.remove_link_class).before(add_link + ' ');
-                // }
-                
-                $(this).remove();
-                
-                // $(obj.get_class()).bind('remove_field.dynamicform', fields_changed);
+                $(target).parents('table').remove();
+                handleFieldUpdate();
             }
             
-            function fields_changed(e) {
-                debug('fields_changed(): fields_changed() was called by ' + $(e.target).attr("class"));
-                debug('fields_changed(): There are ' + obj.count() + ' objects of class ' + obj.get_class() + ' in the DOM.');
+            function handleFieldUpdate(e) {
+                // Handle what happens when a new field is created or removed.
+                debug('handleFieldUpdate() was executed.');
                 
-                var target = $(e.target);
-                var prev_elem = $(e.currentTarget);
+                formClass = '.' + $(obj).find('table').attr("class");
+                var forms = $(formClass).size();
+                debug(forms);
                 
-                // prev_elem.css("background", "red");
-                
-                debug('fields_changed(): target is ' + $(e.target.nodeName));
-                debug('fields_changed(): currentTarget is ' + $(e.currentTarget));
-                
-                if (target.hasClass(options.add_link_class)) {
-                    debug('fields_changed(): add_field() was called');
-                    
-                    if (prev_elem.prev().hasClass(obj.get_class())) {
-                        debug('fields_changed(): this is not the first object');
-                        $(obj.get_class()).prev().find('.' + options.add_link_class + ":last").remove();
-                    } else {
-                        debug('fields_changed(): this is the first object');
-                        prev_elem.find('.' + options.add_link_class).replaceWith(remove_link);
-                        $(obj.get_class()).find('.' + options.add_link_class + ":last").after(' ' + remove_link);
-                    }
+                if (forms == 1) {
+                    $(formClass).find('tr:last > td').empty().append(add_link);
                 }
-                if (target.hasClass(options.remove_link_class)) {
-                    debug('fields_changed(): remove_field() was called');
-                    debug('fields_changed(): objects=' + obj.count());
-                    debug($(obj.get_class() + ':last').find('.' + options.remove_link_class).attr("class"));
-                    
-                    $(e.target).css("background", "green");
-                    $(e.currentTarget).css("background", "purple");
-
-                    if (obj.count() == 1) {
-                        // debug('fields_changed(): ')
-                        obj.find('.' + options.remove_link_class).replaceWith(add_link);
-                        // obj.bind('add_field.dynamicform', add_field);
-                    }
-                    else {
-                        // Check that an add link does not already exist.
-                        // var passed_class = $(obj.get_class() + ':last').find('.' + options.add_link_class));
-                        
-                        debug($(obj.get_class() + ':last').find('.' + options.add_link_class).eq(0).attr("class"));
-                        
-                        if (($(e.currentTarget).prev().find('.' + options.add_link_class).eq(0).attr("class")) != ('.' + options.add_link_class)) {
-                            // debug('fields_changed(): add link before remove link');
-                            // $(e.currentTarget).prev().find('.' + options.remove_link_class).before(add_link + ' ');
-                            // $(obj.get_class() + ':last').find('.' + options.remove_link_class).before(add_link + ' ');
-                        }
-                    }
+                else {
+                    $(formClass).find('tr:last > td').empty().append(remove_link);
+                    $(formClass + ":last").find('.' + options.remove_link_class).before(add_link + ' ');
                 }
             }
-            
-            // Helper methods for dealing with the object
-            obj.get_class = get_class;
-            function get_class() {
-                var class_name = "." + $(this).attr("class");
-                return class_name;
-            }
-            
-            // Return the number of current obj class elements
-            obj.count = count;
-            function count() {
-                var count = $(obj.get_class()).length;
-                return count;
-            }
-            
-            // listEvents(obj);
-            // debug($.event.global);
         });
     }
     
